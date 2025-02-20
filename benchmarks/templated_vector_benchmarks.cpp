e3b798c0e278089899ea8efd8135656da0ea6985
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2025-02-20 12:57:25 +0100

    Vector - Editing append test to only calculate append time and resetting vector at each cycle

--- benchmarks/templated_vector_benchmarks.cpp
+++ benchmarks/templated_vector_benchmarks.cpp
@@ -5,26 +5,30 @@ using myds::Vector;
 
 
 static void BM_VectorAppend(benchmark::State& state) {
-    Vector<int> vec;  // Inizializza un oggetto della classe Vector
-
-    for (auto _ : state) {
-        // Reset del benchmark per evitare side effects tra iterazioni
-        vec = Vector<int>();
-
-        // Esegui il benchmark con il numero di input specificato in state.range(0)
-        for (int i = 0; i < state.range(0); ++i) {
-            vec.append(i);  // Append dei valori al vettore
-        }
+    const size_t initial_size = state.range(0);
+    Vector<int> startingVec(initial_size * 2);
+    for (size_t i = 0; i < initial_size; ++i) {
+        startingVec.append(i); // Populate vec with n elements
     }
 
-    // Facoltativo: registra la dimensione finale del vettore come una controparte
+    for (auto _ : state) {
+        // Reset benchmark condition pausing counting time
+        state.PauseTiming();;
+        // Vector<int> vec(initial_size*3);
+        auto vec = startingVec;
+        state.ResumeTiming();
+
+        // Execute append
+        vec.append(55);
+        benchmark::DoNotOptimize(vec); // Avoid optimization for vec
+    }
     state.SetComplexityN(state.range(0));
 }
 
 // Registra il benchmark e specifica i range di input
 BENCHMARK(BM_VectorAppend)
     ->RangeMultiplier(2)  // Aumenta il numero di input moltiplicandolo per 2
-    ->Range(1 << 10, 1 << 20)  // Da 1024 (2^10) a 1.048.576 (2^20)
+    ->Range(1 << 8, 1 << 20)  // From 256 (2^8) to 1.048.576 (2^20)
     ->Complexity();  // Stima la complessità (es. O(n))
 
 BENCHMARK_MAIN();
