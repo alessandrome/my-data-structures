cb999a31320d6c3c847e28801a9fb0326318bd05
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2025-02-20 14:17:40 +0100

    Vector - prepend and insert benchmarks + log

--- benchmarks/templated_vector_benchmarks.cpp
+++ benchmarks/templated_vector_benchmarks.cpp
@@ -25,10 +25,56 @@ static void BM_VectorAppend(benchmark::State& state) {
     state.SetComplexityN(state.range(0));
 }
 
+static void BM_VectorPrepend(benchmark::State& state) {
+    const size_t initial_size = state.range(0);
+    Vector<int> startingVec(initial_size + 2);
+    for (size_t i = 0; i < initial_size; ++i) {
+        startingVec.append(i); // Populate vec with n elements
+    }
+    for (auto _ : state) {state.PauseTiming();
+        // Pause to reset vector in a starting condition
+        state.PauseTiming();
+        Vector<int> vec = startingVec;
+        state.ResumeTiming();
+
+        vec.prepend(std::rand());
+        benchmark::DoNotOptimize(vec);
+    }
+    state.SetComplexityN(state.range(0));
+}
+
+static void BM_VectorInsert(benchmark::State& state) {
+    const size_t initial_size = state.range(0);
+    Vector<int> startingVec(initial_size + 2);
+    for (size_t i = 0; i < initial_size; ++i) {
+        startingVec.append(i); // Populate vec with n elements
+    }
+    for (auto _ : state) {
+        // Pause n Reset
+        state.PauseTiming();
+        Vector<int> vec = startingVec;
+        state.ResumeTiming();
+
+        vec.insert(42, 1);
+        benchmark::DoNotOptimize(vec);
+    }
+    state.SetComplexityN(state.range(0));
+}
+
 // Registra il benchmark e specifica i range di input
 BENCHMARK(BM_VectorAppend)
     ->RangeMultiplier(2)  // Aumenta il numero di input moltiplicandolo per 2
     ->Range(1 << 8, 1 << 20)  // From 256 (2^8) to 1.048.576 (2^20)
     ->Complexity();  // Stima la complessità (es. O(n))
+
+BENCHMARK(BM_VectorPrepend)
+    ->RangeMultiplier(2)
+    ->Range(1 << 8, 1 << 20)
+    ->Complexity();
+
+BENCHMARK(BM_VectorInsert)
+    ->RangeMultiplier(2)
+    ->Range(1 << 8, 1 << 20)
+    ->Complexity();
 
 BENCHMARK_MAIN();
